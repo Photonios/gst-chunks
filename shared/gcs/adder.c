42d32c32c4dda2191c86c8855299149e06a1f61c
Photonios <photonios@outlook.com>
2015-04-05 17:54:28 +0200

    Due to changes in matroskademux we can link matroskademux directly

--- shared/gcs/adder.c
+++ shared/gcs/adder.c
@@ -5,12 +5,6 @@
 #include <gcs/adder.h>
 
 void
-on_pad_added(GstElement *demuxer, GstPad *pad, GstElement *queue)
-{
-    gst_element_link_many(demuxer, queue, NULL);
-}
-
-void
 gcs_add_chunk_to_pipeline(GstElement *pipeline,
     GstElement *concat_elem, GCS_CHUNK *chunk)
 {
@@ -37,20 +31,10 @@ gcs_add_chunk_to_pipeline(GstElement *pipeline,
 
     g_object_set(source, "location", chunk->full_path, NULL);
 
-    /* we cannot link the demuxer to another element yet since the demuxer
-    uses dynamic pads.. we'll do that later when the pads are created */
-    g_signal_connect(demuxer, "pad-added", G_CALLBACK(on_pad_added), queue);
-
     /* add our bin to the pipeline */
     gst_bin_add_many(GST_BIN(pipeline), bin, NULL);
 
-    /* we do have to link filesrc and matroskademux, otherwise no
-    pads will be created */
-    gst_element_link_many(source, demuxer, NULL);
-
-    /* and we also link all other elements, except the demuxer, it is
-    important we link at least one element to the concat element, because
-    the order in which elements are linked to the concat element matters,
-    if not added in the right other all chunks will be messed up */
-    gst_element_link_many(queue, parser, decoder, concat_elem, NULL);
+    /* link all elements together, and in the end to the concat element */
+    gst_element_link_many(source, demuxer, queue, parser, decoder,
+        concat_elem, NULL);
 }
