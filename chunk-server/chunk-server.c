cc67104951f5d769618409a3d12d03b3b7e697a1
Photonios <photonios@outlook.com>
2015-04-09 09:15:42 +0200

    Started work on chunk-server

---
+++ chunk-server/chunk-server.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+
+#include <gst/gst.h>
+
+#include <gcs/dir.h>
+#include <gcs/chunk.h>
+#include <gcs/index.h>
+#include <gcs/mem.h>
+
+int
+main(int argc, char **argv)
+{
+    /* initialize gstreamer and set the directory that the graphs
+    need to be written to */
+    putenv("GST_DEBUG_DUMP_DOT_DIR=.");
+    gst_init(&argc, &argv);
+
+    return 0;
+}
