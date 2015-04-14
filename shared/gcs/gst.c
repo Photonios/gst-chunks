d8e6fde57572faf28a4e792e8ab5f44fa677a232
Photonios <photonios@outlook.com>
2015-04-14 23:23:25 +0200

    Added function for replacing an element in a bin

---
+++ shared/gcs/gst.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include <gst/gst.h>
+
+#include <gcs/mem.h>
+#include <gcs/gst.h>
+
+int
+gcs_gst_replace_element(GstElement *bin, GstElement *left_element,
+    GstElement *right_element, GstElement *old_element,
+    GstElement *new_element)
+{
+    if(!bin || !old_element || !new_element) {
+        return 0;
+    }
+
+    if(!left_element && !right_element) {
+        return 0;
+    }
+
+    if(left_element) {
+        gst_element_unlink(left_element, old_element);
+    }
+
+    if(right_element) {
+        gst_element_unlink(old_element, right_element);
+    }
+
+    gst_bin_remove(GST_BIN(bin), old_element);
+    gst_bin_add(GST_BIN(bin), new_element);
+
+    if(left_element) {
+        gst_element_link(left_element, new_element);
+    }
+
+    if(right_element) {
+        gst_element_link(new_element, right_element);
+    }
+
+    return 1;
+}
